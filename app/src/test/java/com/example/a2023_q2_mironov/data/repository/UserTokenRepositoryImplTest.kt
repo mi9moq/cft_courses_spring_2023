6d927d232c8a639b2f46d6628b91fc1787fbf076
Миронов Денис Владимирович <denis0799_97@mail.ru>
2023-06-17 00:10:53 +0700

    Добавил тесты для UserTokenRepositoryImpl

---
+++ app/src/test/java/com/example/a2023_q2_mironov/data/repository/UserTokenRepositoryImplTest.kt
@@ -0,0 +1,36 @@
+package com.example.a2023_q2_mironov.data.repository
+
+import com.example.a2023_q2_mironov.data.datasource.TokenLocalDataSource
+import com.example.a2023_q2_mironov.domain.entity.AccessUserToken
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+class UserTokenRepositoryImplTest {
+
+    private val dataSource: TokenLocalDataSource = mock()
+    private val repository = UserTokenRepositoryImpl(dataSource)
+
+    private val testToken = AccessUserToken("user token")
+
+    @Test
+    fun `getUserToken EXPECT AccessUserToken`(){
+        whenever(dataSource.get()) doReturn testToken
+
+        val expected = testToken
+        val actual = repository.getUserToken()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `setUserToken EXPECT set to preferences`(){
+
+        repository.setUserToken(testToken)
+
+        verify(dataSource).set(testToken)
+    }
+}

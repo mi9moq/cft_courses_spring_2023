4e172e386f488b85f3354663dad44385be8bdc57
Миронов Денис Владимирович <denis0799_97@mail.ru>
2023-06-17 23:22:52 +0700

    Рефактор

--- app/src/main/java/com/example/a2023_q2_mironov/navigation/router/WelcomeRouter.kt
+++ app/src/main/java/com/example/a2023_q2_mironov/navigation/router/WelcomeRouter.kt
@@ -1,5 +1,9 @@
 package com.example.a2023_q2_mironov.navigation.router
 
+import com.example.a2023_q2_mironov.navigation.screen.getLoginScreen
+import com.github.terrakok.cicerone.Router
+import javax.inject.Inject
+
 interface WelcomeRouter {
 
     fun openLogin()
@@ -7,4 +11,21 @@ interface WelcomeRouter {
     fun openRegistration()
 
     fun openMain()
+}
+
+class WelcomeRouterImpl @Inject constructor(
+    private val router: Router
+) : WelcomeRouter {
+
+    override fun openLogin() {
+        router.navigateTo(getLoginScreen())
+    }
+
+    override fun openRegistration() {
+        TODO("Not yet implemented")
+    }
+
+    override fun openMain() {
+        TODO("Not yet implemented")
+    }
 }

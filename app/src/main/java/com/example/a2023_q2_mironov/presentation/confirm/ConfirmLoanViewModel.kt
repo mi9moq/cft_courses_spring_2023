15cebbeb3c63979f9e1189c9dee913b9b0fa7ac6
Миронов Денис Владимирович <denis0799_97@mail.ru>
2023-06-20 23:22:29 +0700

    Добавил обработку стейтов

--- app/src/main/java/com/example/a2023_q2_mironov/presentation/confirm/ConfirmLoanViewModel.kt
+++ app/src/main/java/com/example/a2023_q2_mironov/presentation/confirm/ConfirmLoanViewModel.kt
@@ -8,8 +8,17 @@ import com.example.a2023_q2_mironov.domain.entity.LoanRequest
 import com.example.a2023_q2_mironov.domain.usecase.CreateLoanUseCase
 import com.example.a2023_q2_mironov.domain.usecase.GetUserTokenUseCase
 import com.example.a2023_q2_mironov.navigation.router.ConfirmRouter
-import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanState.*
+import com.example.a2023_q2_mironov.presentation.ErrorType
+import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanState.Content
+import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanState.Error
+import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanState.Initial
+import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanState.Loading
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class ConfirmLoanViewModel @Inject constructor(
@@ -21,6 +30,22 @@ class ConfirmLoanViewModel @Inject constructor(
     private val _state: MutableLiveData<ConfirmLoanState> = MutableLiveData(Initial)
     val state: LiveData<ConfirmLoanState> = _state
 
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        when (throwable) {
+            is UnknownHostException, is SocketTimeoutException, is ConnectException -> _state.value =
+                Error(ErrorType.CONNECTION)
+
+            is HttpException -> {
+                if (throwable.code() == 401)
+                    _state.value = Error(ErrorType.UNAUTHORIZED)
+                else if (throwable.code() == 404)
+                    _state.value = Error(ErrorType.NOT_FOUND)
+            }
+
+            else -> _state.value = Error(ErrorType.UNKNOWN)
+        }
+    }
+
     private lateinit var request: LoanRequest
 
     fun putLoanRequest(loanRequest: LoanRequest) {
@@ -30,7 +55,8 @@ class ConfirmLoanViewModel @Inject constructor(
 
     fun createLoan() {
         val token = getUserTokenUseCase().userToken
-        viewModelScope.launch {
+        viewModelScope.launch(handler) {
+            _state.value = Loading
             createLoanUseCase(token, request)
             router.openApproved()
         }
--- app/src/main/java/com/example/a2023_q2_mironov/ui/ConfirmLoanFragment.kt
+++ app/src/main/java/com/example/a2023_q2_mironov/ui/ConfirmLoanFragment.kt
@@ -7,10 +7,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.a2023_q2_mironov.R
 import com.example.a2023_q2_mironov.databinding.FragmentConfirmLoanBinding
 import com.example.a2023_q2_mironov.domain.entity.LoanRequest
+import com.example.a2023_q2_mironov.presentation.ErrorType
 import com.example.a2023_q2_mironov.presentation.ViewModelFactory
 import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanState.*
 import com.example.a2023_q2_mironov.presentation.confirm.ConfirmLoanViewModel
@@ -78,9 +81,9 @@ class ConfirmLoanFragment : Fragment() {
         viewModel.state.observe(viewLifecycleOwner) { state ->
             when (state) {
                 Initial -> Unit
-                Loading -> Unit //TODO
+                Loading -> launchLoadingState()
                 is Content -> launchContentState(state.loanRequest)
-                is Error -> Unit //TODO
+                is Error -> launchErrorState(state.type)
             }
         }
     }
@@ -93,6 +96,43 @@ class ConfirmLoanFragment : Fragment() {
             percentValue.text = request.percent.toString()
             phoneNumberValue.text = request.phoneNumber
         }
+    }
+
+    private fun launchLoadingState() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.container.visibility = View.GONE
+    }
+
+    private fun launchErrorState(type: ErrorType) {
+        binding.progressBar.visibility = View.GONE
+        binding.container.visibility = View.GONE
+        when (type) {
+            ErrorType.UNAUTHORIZED -> {
+                val message = getString(R.string.authorisation_error)
+                showToast(message)
+            }
+
+            ErrorType.NOT_FOUND -> {
+                val message = getString(R.string.not_found_error)
+                showToast(message)
+            }
+
+            ErrorType.UNKNOWN -> {
+                val message = getString(R.string.unknown_error)
+                showToast(message)
+            }
+
+            ErrorType.CONNECTION -> {
+                val message = getString(R.string.connection_error)
+                showToast(message)
+            }
+
+            ErrorType.REGISTRATION -> Unit
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
     }
 
     @Suppress("DEPRECATION")
